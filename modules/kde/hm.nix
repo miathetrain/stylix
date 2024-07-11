b2fb8f7b7bab029faef85feda0048cb7431072c4
Miah <59005594+miathetrain@users.noreply.github.com>
2024-07-11 01:29:10 -0400

    Update hm.nix
    remove kde wallpaper live-updating

--- modules/kde/hm.nix
+++ modules/kde/hm.nix
@@ -236,38 +236,5 @@ in {
     xdg.configFile."kdeglobals".text = "${formatConfig colorscheme}";
 
     xdg.systemDirs.config = [ "${configPackage}" ];
-
-    # plasma-apply-wallpaperimage is necessary to change the wallpaper
-    # after the first login.
-    #
-    # plasma-apply-lookandfeel is only here to trigger a hot reload, the theme
-    # would still be applied without it if you logged out and back in.
-    #
-    # Home Manager clears $PATH before running the activation script, but we
-    # want to avoid installing these tools explicitly because that would pull
-    # in large dependencies for people who aren't actually using KDE.
-    # The workaround used is to assume a list of common paths where the tools
-    # might be installed, and look there. The ideal solution would require
-    # changes to KDE to make it possible to update the wallpaper through
-    # config files alone.
-    home.activation.stylixLookAndFeel = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      global_path() {
-        for directory in /run/current-system/sw/bin /usr/bin /bin; do
-          if [[ -f "$directory/$1" ]]; then
-            printf '%s\n' "$directory/$1"
-            return 0
-          fi
-        done
-
-        return 1
-      }
-
-      if wallpaper_image="$(global_path plasma-apply-wallpaperimage)"; then
-        "$wallpaper_image" "${themePackage}/share/wallpapers/stylix"
-      else
-        verboseEcho \
-          "plasma-apply-wallpaperimage: command not found"
-      fi
-    '';
   };
 }
